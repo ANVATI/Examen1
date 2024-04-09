60a3709700883dae462b62a46ae9a677ff72b265
unknown <antoniotisza@gmail.com>
2024-04-09 11:38:08 -0500

    Enemigo Patrullaje

---
+++ Assets/Enemie.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemie : MonoBehaviour
+{
+    [SerializeField] private Transform[] checkpointsPatrol;
+    [SerializeField] private Rigidbody myRBD;
+    [SerializeField] private float velocityModifier = 5.0f;
+    private Transform currentPositionTarget;
+    private int patrolPos = 0;
+
+    private void Start()
+    {
+        currentPositionTarget = checkpointsPatrol[patrolPos];
+        transform.position = currentPositionTarget.position;
+    }
+
+    private void Update()
+    {
+        CheckNewPoint();
+    }
+
+    private void CheckNewPoint()
+    {
+        if (Mathf.Abs((transform.position - currentPositionTarget.position).magnitude) < 0.25)
+        {
+            patrolPos = (patrolPos + 1) % checkpointsPatrol.Length;
+            currentPositionTarget = checkpointsPatrol[patrolPos];
+            myRBD.velocity = (currentPositionTarget.position - transform.position).normalized * velocityModifier;
+        }
+    }
+}
